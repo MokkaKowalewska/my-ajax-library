cee3ef817cac59ab094ba1ec57f7d5f117a06a44
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-04 23:53:05 +0200

    add extendOptions method

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,52 @@
 function AJAX(config) {
+  if (!(this instanceof AJAX)) {
+    return new AJAX(config);
+  }
 
-    if( !(this instanceof AJAX)) {
-        return new AJAX(config);
+  this._xhr = new XMLHttpRequest();
+  this._config = this._extendOptions(config);
+
+  console.log(this._config);
+}
+
+AJAX.prototype._extendOptions = function(config) {
+  const defeaultConfig = JSON.parse(JSON.stringify(this._defeaultConfig));
+
+  for (let key in defeaultConfig) {
+
+    if (key in config) {
+      defeaultConfig[key] = config[key];
     }
+  }
+};
 
-    this._xhr = new XMLHttpRequest();
-    this._config = this._extendOptions(config);
- }
-
- AJAX.prototype._extendOptions = function(config) {
-
-    for(let key in this._defeaultConfig) {
-        console.log(key);
-    }
- }
-
- var a = AJAX.prototype._defeaultConfig = {
-    type: "GET",
-    url: window.location.href,
-    data: {},
-    options: {
-        async: true,
-        timeout: 0,
-        username: null,
-        password: null
-    },
-    headers: {}
- };
-
-
- AJAX({
-    type: "POST",
-    url: "odbierz.php",
-    data: {
-        firstName: "Piotr",
-        lastName: "Kowalski"
-    },
-    headers: {
-        "X-My-Header": "123#asdf"
-    },
-    success: function(response, xhr) {
-        console.log("Udało się! Status: " + xhr.status);
-    },
-    failure: function(xhr) {
-        console.log("Wystąpił błąd. Status: " + xhr.status);
-    }
+var a = (AJAX.prototype._defeaultConfig = {
+  type: "GET",
+  url: window.location.href,
+  data: {},
+  options: {
+    async: true,
+    timeout: 0,
+    username: null,
+    password: null,
+  },
+  headers: {},
 });
 
+AJAX({
+  type: "POST",
+  url: "odbierz.php",
+  data: {
+    firstName: "Piotr",
+    lastName: "Kowalski",
+  },
+  headers: {
+    "X-My-Header": "123#asdf",
+  },
+  success: function (response, xhr) {
+    console.log("Udało się! Status: " + xhr.status);
+  },
+  failure: function (xhr) {
+    console.log("Wystąpił błąd. Status: " + xhr.status);
+  },
+});
