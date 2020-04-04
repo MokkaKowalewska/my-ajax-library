c6f63937ef37b34d8ce47406daac1098c93a5c9b
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-04 23:31:24 +0200

    Add AJAX function, defeault config

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,35 @@
-/*
+function AJAX(config) {
 
-AJAX({
+    if( !(this instanceof AJAX)) {
+        return new AJAX(config);
+    }
+
+    this._xhr = new XMLHttpRequest();
+    this._config = this._extendOptions(config);
+ }
+
+ AJAX.prototype._extendOptions = function(config) {
+
+    for(let key in this._defeaultConfig) {
+        console.log(key);
+    }
+ }
+
+ var a = AJAX.prototype._defeaultConfig = {
+    type: "GET",
+    url: window.location.href,
+    data: {},
+    options: {
+        async: true,
+        timeout: 0,
+        username: null,
+        password: null
+    },
+    headers: {}
+ };
+
+
+ AJAX({
     type: "POST",
     url: "odbierz.php",
     data: {
@@ -18,4 +47,3 @@ AJAX({
     }
 });
 
- */
