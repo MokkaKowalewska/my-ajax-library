6e02d5064dfc6314c164622bfd479c99f3eb01b1
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-05 10:10:33 +0200

    add assignEvents, assignUserHeaders, open and send methods

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,11 @@ function AJAX(config) {
 
   this._xhr = new XMLHttpRequest();
   this._config = this._extendOptions(config);
-
-  console.log(this._config);
+  this._assignEvents();
+  this._assignUserHeaders();
+  this._open();
+  this._send();
 }
-
-AJAX.prototype._extendOptions = function (config) {
-  var defaultConfig = JSON.parse(JSON.stringify(this._defaultConfig));
-
-  for (var key in defaultConfig) {
-    if (key in config) {
-      defaultConfig[key] = config[key];
-    }
-  }
-
-  return defaultConfig;
-};
 
 AJAX.prototype._defaultConfig = {
   type: "GET",
@@ -33,6 +23,74 @@ AJAX.prototype._defaultConfig = {
   },
   headers: {},
 };
+
+AJAX.prototype._extendOptions = function (config) {
+  var defaultConfig = JSON.parse(JSON.stringify(this._defaultConfig));
+
+  for (var key in defaultConfig) {
+    if (key in config) {
+      defaultConfig[key] = config[key];
+    }
+  }
+
+  return defaultConfig;
+};
+
+AJAX.prototype._assignEvents = function () {
+  this._xhr.addEventListener(
+    "readystatechange",
+    this._handleResponse.bind(this),
+    false
+  );
+
+  this._xhr.addEventListener("abort", this._handleError.bind(this), false);
+
+  this._xhr.addEventListener("error", this._handleError.bind(this), false);
+
+  this._xhr.addEventListener("timeout", this._handleError.bind(this), false);
+};
+
+AJAX.prototype._assignUserHeaders = function () {
+  if((Object.keys(this._config.headers)).length) {
+
+    for(let key in this._config.headers) {
+        this._xhr.setRequestHeader(key, this._config.headers[key]);
+    }
+
+  }
+};
+
+AJAX.prototype._open = function () {
+
+    this._xhr.open(
+        this._config.type,
+        this._config.url,
+        this._config.options.async,
+        this._config.options.username,
+        this._config.options.password,
+
+        );
+
+        this._xhr.timeout = this._config.options.timeout;
+
+};
+
+AJAX.prototype._send = function () {
+
+    this._xhr.send();
+}
+
+AJAX.prototype._handleResponse = function (e) {
+
+    if(this._xhr.readyState === 4 && this._xhr.status === 200) {
+
+        console.log("Otrzymano odpowiedź");
+
+    }
+
+};
+
+AJAX.prototype._handleError = function (e) {};
 
 AJAX({
   type: "POST",
